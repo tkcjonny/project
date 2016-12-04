4758dd21156fd16f0c7b19eda0c7eccbe988afd9
tkcjonny <tkcjonny@gmail.com>
2016-12-05 06:26:13 +0800

    Update server_post.js

--- server_post.js
+++ server_post.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var mongodburl = 'mongodb://tkcjonny:xiao5354@ds111788.mlab.com:11788/restaurant';
+var mongodburl = 'mongodb://id:pw@ds111788.mlab.com:11788/restaurant';
 var restSchema = require('./models/restaurant');
 var bodyParser = require('body-parser');
 
